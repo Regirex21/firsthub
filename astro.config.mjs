28a79c592ff79f90bb1b6dc7e9a89d6885589c9e
rex <157132770+Regirex21@users.noreply.github.com>
2026-04-21 22:35:58 -0600

    cambiso en los links de docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,12 @@ import starlight from "@astrojs/starlight";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  site: "http://localhost:4321",
   integrations: [
     starlight({
       title: "FIRSTHub Docs"
     })
   ],
-
   vite: {
     plugins: [tailwindcss()]
   }
